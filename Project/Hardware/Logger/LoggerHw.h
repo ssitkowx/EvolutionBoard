7e5615423f2304deb7e84314677772975271dde4
sylsit <sitkowskis@interia.pl>
2020-07-18 10:01:37 +0200

    Folders structure deep refactor.

--- Project/Hardware/Logger/LoggerHw.h
+++ Project/Hardware/Logger/LoggerHw.h
@@ -16,12 +16,12 @@
 /////////////////////////// MACROS/DEFINITIONS ////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-#define LOG(MODULE,MSG, ...)  Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eLogNone   , MODULE, MSG, ##__VA_ARGS__);
-#define LOGE(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eLogError  , MODULE, MSG, ##__VA_ARGS__);
-#define LOGW(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eLogWarning, MODULE, MSG, ##__VA_ARGS__);
-#define LOGI(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eLogInfo   , MODULE, MSG, ##__VA_ARGS__);
-#define LOGD(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eLogDebug  , MODULE, MSG, ##__VA_ARGS__);
-#define LOGV(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eLogVerbose, MODULE, MSG, ##__VA_ARGS__);
+#define LOG(MODULE,MSG, ...)  Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eNone   , MODULE, MSG, ##__VA_ARGS__);
+#define LOGE(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eError  , MODULE, MSG, ##__VA_ARGS__);
+#define LOGW(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eWarning, MODULE, MSG, ##__VA_ARGS__);
+#define LOGI(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eInfo   , MODULE, MSG, ##__VA_ARGS__);
+#define LOGD(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eDebug  , MODULE, MSG, ##__VA_ARGS__);
+#define LOGV(MODULE,MSG, ...) Logger::GetInstance ()->Log <LoggerHw> (Logger::ELogLevel::eVerbose, MODULE, MSG, ##__VA_ARGS__);
 
 ///////////////////////////////////////////////////////////////////////////////
 /////////////////////////// CLASSES/STRUCTURES ////////////////////////////////
@@ -34,12 +34,12 @@ class LoggerHw : public Logger
         {
             switch (v_eLogLevel)
             {
-                case ELogLevel::eLogNone:    { return LOG_COLOR_BLACK;  }
-                case ELogLevel::eLogError:   { return LOG_COLOR_RED;    }
-                case ELogLevel::eLogWarning: { return LOG_COLOR_CYAN;   }
-                case ELogLevel::eLogInfo:    { return LOG_COLOR_BLUE;   }
-                case ELogLevel::eLogDebug:   { return LOG_COLOR_PURPLE; }
-                case ELogLevel::eLogVerbose: { return LOG_COLOR_GREEN;  }
+                case ELogLevel::eNone:    { return LOG_COLOR_BLACK;  }
+                case ELogLevel::eError:   { return LOG_COLOR_RED;    }
+                case ELogLevel::eWarning: { return LOG_COLOR_CYAN;   }
+                case ELogLevel::eInfo:    { return LOG_COLOR_BLUE;   }
+                case ELogLevel::eDebug:   { return LOG_COLOR_PURPLE; }
+                case ELogLevel::eVerbose: { return LOG_COLOR_GREEN;  }
                 default:                     { break;                   }
             };
 
--- README.md
+++ README.md
@@ -14,40 +14,35 @@ The code stored here is environment-specific, which means:
 - The code can use both generic and evironment-specific external libraries.
 
 # III. Structure:
-The embedded project has been divided into two parts:
+The embedded project has been divided into three parts:
+
+External:
+- This is where external generic libraries are stored,
+- This is where documents and resources are stored.
 
 Logic:
-- This is where external generic libraries are stored,
 - This is where environment-specific but hardware-independent code is stored.
 
 Hardware:
-- This is where the environment-specific code is kept.
+- This is where the environment-specific code is stored.
 
 # IV. Configuration:
-Logic:
-- The same as in the case of projects based on the "Template".
 
-Hardware:
-- Eclipse IDE for C/C++ Developers. Version: 2019-12(4.14.0),
+- Eclipse IDE for C/C++ Developers. Version: 2020-06(4.16.0),
 - ESP-IDF Branch_v4.0. Eclipse plugin configured according https://github.com/espressif/idf-eclipse-plugin/blob/master/README.md
 - Generate hal libraries from CubeMx version 5.3.0,
 
 # V. Builidng:
-Logic:
+
+Packages in External:
 - The same as in the case of projects based on the "Template".
 
-Hardware:
-- Add to the project files located in the ".conan\download" after "Logic" building,
-- Add to the project files located in the "Hardware",
+Project:
+- Add to the project (in the CMakeLists.txt) files located in the ".conan\download" in the "External" folder,
+- Add to the project (in the CMakeLists.txt) files located in the "Logic" and "Hardware" folders,
 - Open a project gui and build a project.
 
 # VI. Tips:
-- Exclude 'main' from the build logic project,
-- To enable C++ settings add <nature>org.eclipse.cdt.core.ccnature</nature> to .project.
-- After generating the equipment using CubeMx, transfer the .cproject, .project, .mxproject files from the Atollic folder to the folder with the project name (AtollicTemplate). 
-  - In the C/C++ Build->Settings->Settings->Tool Settings->C++ Linker->General settings we provide the path to the linker script,
-  - In C/C++ Build-> Target Settings we choose the type of processor,
-  - In C/C++ General->Paths and Symbols->Include, we provide the hardware paths updates,
 - Remove '--no-site-packages' from idf_tool.py:
   - from: subprocess.check_call([sys.executable, '-m', 'virtualenv', '--no-site-packages', idf_python_env_path],
   - to:   subprocess.check_call([sys.executable, '-m', 'virtualenv', idf_python_env_path],
