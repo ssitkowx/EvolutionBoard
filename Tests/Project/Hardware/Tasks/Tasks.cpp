95df4f81f00f1412440d4aaae210e5879ad43caa
sylsit <sitkowskis@interia.pl>
2021-03-28 20:03:40 +0200

    WeatherMeasureComm added. Common MainCppHw and separated Tasks in Project and Tests.

---
+++ Tests/Project/Hardware/Tasks/Tasks.cpp
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// INCLUDES /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Tasks.h"
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// FUNCTIONS ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+void Tasks::BluetoothProcess (void * v_params)
+{
+    while (IsThreadInProgress ())
+    {
+        Rtos::GetInstance ()->DelayInMs (TEN);
+    }
+
+    Rtos::GetInstance ()->TaskDelete ();
+    LOGE              (MODULE, "BluetoothProcess() Deleted.");
+}
+
+void Tasks::WeatherMeasureProcess (void * v_params)
+{
+    while (IsThreadInProgress ())
+    {
+        if (Rtos::GetInstance ()->TakeSemaphore ("TakeWeatherMeasureSemaphore") == true)
+        {
+            WeatherMeasureComm.Process ();
+        }
+    }
+
+    Rtos::GetInstance ()->TaskDelete ();
+    LOGE              (MODULE, "WeatherMeasureProcess() Deleted.");
+
+}
+
+void Tasks::DisplayAndTouchProcess (void * v_params)
+{
+    while (IsThreadInProgress ())
+    {
+        Action.Process ();
+        Rtos::GetInstance ()->DelayInMs (TEN);
+    }
+
+    Rtos::GetInstance ()->TaskDelete ();
+    LOGE              (MODULE, "DisplayAndTouchProcess() Deleted.");
+}
+
+void Tasks::Process (void)
+{
+    std::unique_ptr<std::thread> bluetoothThread       (new std::thread ([this] { BluetoothProcess       (NULL); }));
+    std::unique_ptr<std::thread> watherMeasureThread   (new std::thread ([this] { WeatherMeasureProcess  (NULL); }));
+    std::unique_ptr<std::thread> displayAndTouchThread (new std::thread ([this] { DisplayAndTouchProcess (NULL); }));
+
+    bluetoothCommThread       = std::move (bluetoothThread);
+    watherMeasureCommThread   = std::move (watherMeasureThread);
+    displayAndTouchCommThread = std::move (displayAndTouchThread);
+
+    bluetoothCommThread      ->join ();
+    watherMeasureCommThread  ->join ();
+    displayAndTouchCommThread->join ();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// END OF FILE ///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
---
+++ Tests/Project/Logic/WeatherMeasure/WeatherMeasureFixture.cxx
@@ -0,0 +1,138 @@
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// INCLUDES /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+#include "WeatherMeasureFixture.hxx"
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// VARIABLES ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+using ::testing::_;
+using ::testing::DoAll;
+using ::testing::Return;
+using ::testing::AnyNumber;
+
+static std::string_view weatherMeasureResp = "{\"request\": {\
+                                                     \"type\": \"City\",\
+                                                     \"query\" : \"Warsaw, Poland\",\
+                                                     \"language\" : \"en\",\
+                                                     \"unit\" : \"m\"\
+                                                   },\
+                                                 \"location\": {\
+                                                     \"name\": \"Warsaw\",\
+                                                     \"country\" : \"Poland\",\
+                                                     \"region\" : \"\",\
+                                                     \"lat\" : \"52.250\",\
+                                                     \"lon\" : \"21.000\",\
+                                                     \"timezone_id\" : \"Europe/Warsaw\",\
+                                                     \"localtime\" : \"2020-10-31 18:55\",\
+                                                     \"localtime_epoch\" : 1604170500,\
+                                                     \"utc_offset\" : \"1.0\"\
+                                                   },\
+                                                   \"current\": {\
+                                                       \"observation_time\": \"05:55 PM\",\
+                                                       \"temperature\" : 7,\
+                                                       \"weather_code\" : 143,\
+                                                       \"weather_icons\" : [\
+                                                       \"https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0006_mist.png\"\
+                                                       ] ,\
+                                                       \"weather_descriptions\" : [\
+                                                           \"Mist\",\
+                                                           \"Sun\"\
+                                                       ] ,\
+                                                       \"wind_speed\" : 6,\
+                                                       \"wind_degree\" : 270,\
+                                                       \"wind_dir\" : \"W\",\
+                                                       \"pressure\" : 1025,\
+                                                       \"precip\" : 0.1,\
+                                                       \"humidity\" : 100,\
+                                                       \"cloudcover\" : 75,\
+                                                       \"feelslike\" : 6,\
+                                                       \"uv_index\" : 3,\
+                                                       \"visibility\" : 6,\
+                                                       \"is_day\" : \"yes\"\
+                                            }\}";
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// FUNCTIONS ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+ACTION_TEMPLATE (SetCloudResponse, HAS_1_TEMPLATE_PARAMS (char, ARG_NUM), AND_2_VALUE_PARAMS (v_cloudResp, v_len))
+{
+    memcpy ((char *)std::get<ARG_NUM> (args), (char *)v_cloudResp, v_len);
+}
+
+TEST_F (WeatherMeasureFixture, DownloadAndCompareWeatherData)
+{
+    LOGW                        (MODULE, "DownloadAndCompareWeatherData");
+
+    EXPECT_CALL                 (RtosHw      , DelayInMs           (_))                                                     .Times          (AnyNumber ());
+    EXPECT_CALL                 (RtosHw      , TaskDelete          ())                                                      .Times          (THREE);
+    EXPECT_CALL                 (RtosHw      , GetCurrentHeapSize  ())                                                      .Times          (AnyNumber ());
+    EXPECT_CALL                 (RtosHw      , GetCurrentStackSize (_))                                                     .Times          (AnyNumber ());
+    EXPECT_CALL                 (DraftsmanHw , DrawText            (_, _))                                                  .Times          (AnyNumber ());
+    EXPECT_CALL                 (DraftsmanHw , sendLines           (_))                                                     .Times          (AnyNumber ());
+    EXPECT_CALL                 (HttpClientHw, Cleanup             ())                                                      .Times          (AnyNumber ());
+    EXPECT_CALL                 (HttpClientHw, GetStatusCode       ())                                                      .Times          (AnyNumber ());
+    EXPECT_CALL                 (TouchHw     , isTouched           ())                                                      .Times          (AnyNumber ());
+
+    WiFiHw::Mode.StaConnected = true;
+    WeatherMeasureComm.SetState (WeatherMeasureComm::EState::eReceive);
+    EXPECT_CALL                 (RtosHw      , TakeSemaphore (static_cast<std::string_view>("TakeWeatherMeasureSemaphore"))).WillOnce       (Return    (true))
+                                                                                                                            .WillRepeatedly (Return    (false));
+    EXPECT_CALL                 (HttpClientHw, Init                ())                                                      .WillOnce       (Return    (true))
+                                                                                                                            .WillRepeatedly (Return    (false));
+    EXPECT_CALL                 (HttpClientHw, Open                (ZERO))                                                  .WillOnce       (Return    (ZERO))
+                                                                                                                            .WillRepeatedly (Return    (ZERO));
+    EXPECT_CALL                 (HttpClientHw, FetchHeaders        ())                                                      .WillOnce       (Return    (weatherMeasureResp.size ()))
+                                                                                                                            .WillRepeatedly (Return    (ZERO));
+    EXPECT_CALL                 (HttpClientHw, Read                (_, _))                                                  .WillOnce       (DoAll     (SetCloudResponse <ZERO> (weatherMeasureResp.data(), weatherMeasureResp.size ()), Return (weatherMeasureResp.size ())));
+    EXPECT_CALL                 (*this, IsThreadInProgress         ())                                                      .WillOnce       (Return    (true))
+                                                                                                                            .WillOnce       (Return    (true))
+                                                                                                                            .WillOnce       (Return    (true))
+                                                                                                                            .WillOnce       (Return    (true))
+                                                                                                                            .WillOnce       (Return    (true))
+                                                                                                                            .WillOnce       (Return    (true))
+                                                                                                                            .WillOnce       (Return    (true))
+                                                                                                                            .WillRepeatedly (Return    (false));
+    
+    
+
+    Process ();
+
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Request.Type           .data (), "City"            );
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Request.Query          .data (), "Warsaw, Poland"  );
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Request.Language       .data (), "en"              );
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Request.Unit           .data (), "m"               );
+
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Location.Name          .data (), "Warsaw"          );
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Location.Country       .data (), "Poland"          );
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Location.Region        .data (), ""                );
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Location.Lat           .data (), "52.250"          );
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Location.Lon           .data (), "21.000"          );
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Location.TimezonId     .data (), "Europe/Warsaw"   );
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Location.LocalTime     .data (), "2020-10-31 18:55");
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Location.LocalTimeEpoch, 1604170500);
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Location.UtcOffset     .data (), "1.0");
+
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Current.ObservationTime.data (), "05:55 PM");
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.Temperature, 7);
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.WeatherCode, 143);
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.WindSpeed  , 6);
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.WindDegree , 270);
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Current.WindDir        .data (), "W");
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.Pressure   , 1025);
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.Precip     , 0.1);
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.Humidity   , 100);
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.CloudCover , 75);
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.FeelsLike  , 6);
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.UvIndex    , 3);
+    EXPECT_EQ                   (Settings::GetInstance ().WeatherMeasureMsgType.Current.Visibility , 6);
+    EXPECT_STREQ                (Settings::GetInstance ().WeatherMeasureMsgType.Current.IsDay          .data (), "yes");
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// END OF FILE ///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
